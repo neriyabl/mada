34ff14dab53ffd3a9735984162d6332bcc9084ef
Neriya Bar-Lev <neriyabl555@gmail.com>
2020-06-24 21:46:29 +0300

    add table and upload csv option

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,151 +1,97 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-card class="ma-7" elevation="4">
+    <v-card-title class="py-0 accent">
+      <v-row>
+        <v-col cols="4" lg="2" class="pa-1">
+          <v-file-input
+            @change="handleFileUpload()"
+            v-model="file"
+            label="העלאת קובץ csv"
+            accept=".csv"
+          />
+        </v-col>
+      </v-row>
+    </v-card-title>
+    <v-divider v-if="fileData.length" />
+    <v-card-text v-if="fileData.length">
+      <v-row>
+        <v-col cols="12">
+          <v-data-table
+            v-model="selectedPatients"
+            :headers="headers"
+            :items="fileData"
+            show-select
+            item-key="id"
+          />
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
+export default {
+  name: "HelloWorld",
 
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/layout/pre-defined',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
-    }),
+  data: () => ({
+    file: null,
+    fileData: [],
+    selectedPatients: [],
+    headers: [
+      {
+        text: "תעודת זהות",
+        align: "start",
+        sortable: false,
+        value: "id"
+      },
+      { text: "שם פרטי", value: "firstName" },
+      { text: "שם משפחה", value: "lastName" },
+      { text: "טלפון", value: "phoneNumber" },
+      { text: "נייד", value: "cellphoneNumber" },
+      { text: "יישוב", value: "city" },
+      { text: "רחוב", value: "street" },
+      { text: "מספר בית", value: "houseNumber" }
+    ]
+  }),
+  methods: {
+    handleFileUpload() {
+      if (!this.file) {
+        this.fileData = [];
+        return;
+      }
+      // const file = this.$refs.file.files[0];
+      if (!this.file.name.endsWith(".csv")) {
+        return;
+      }
+      const reader = new FileReader();
+      reader.addEventListener("loadend", ({ target: { result } }) => {
+        if (!this.validateFileData(result)) {
+          alert("הקובץ לא תקין");
+          return;
+        }
+        this.fileData = result
+          .split("\n")
+          .filter(f => Boolean(f) && f !== "\n")
+          .map(row => {
+            const props = row.split(",");
+            return {
+              id: props[0],
+              firstName: props[1],
+              lastName: props[2],
+              phoneNumber: props[3],
+              cellphoneNumber: props[4],
+              city: props[5],
+              street: props[6],
+              houseNumber: props[7]
+            };
+          });
+      });
+      reader.readAsText(this.file);
+    },
+    validateFileData(data) {
+      //todo validate file data
+      return data || true;
+    }
   }
+};
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,16 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  rtl: true,
+  theme: {
+    themes: {
+      light: {
+        accent: colors.lightBlue.lighten5
+      }
+    }
+  }
 });
